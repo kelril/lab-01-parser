0d70acde59bd405960a78b4e91c1d8f21fa12284
MIB <48214233+DarthBarada@users.noreply.github.com>
2019-09-16 11:02:42 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -8,7 +8,7 @@ int main()
 		Json b("{\"menu\": \"Файл\",\"commands\":[{\"title\":\"Новый\",\"action\":\"CreateDoc\"},{\"title\":\"Открыть\",\"action\":\"OpenDoc\"},{\"title\":\"Закрыть\",\"action\": \"CloseDoc\"}]}");
 		Json a("Json,json");
 		a.print();
-		std::cout << R("//---------------------------------\n");
+		std::cout << "//---------------------------------\n";
 		b.print();
 		setlocale(LC_ALL, "ru-RU");
 		return 0;
