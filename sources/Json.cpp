c420ca4517a3e0fb46b8691c29aedde20dff04c7
MIB <48214233+DarthBarada@users.noreply.github.com>
2019-09-16 17:22:36 +0300

    Update Json.cpp

--- sources/Json.cpp
+++ sources/Json.cpp
@@ -389,7 +389,7 @@ std::any return_object(std::stack <std::any>&);
 			 {
 				 return false;
 			 }
-		 if (s.front() > -1 && s.front() < 255)
+		 if (int(s.front()) > 47 && int(s.front()) < 58)
 			 {
 				 if (isdigit(s.front() - '\0'))
 					 {
