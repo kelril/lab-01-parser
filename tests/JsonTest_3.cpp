2c500d685e320f4437bd87218b8bfea8eb965391
MIB <48214233+DarthBarada@users.noreply.github.com>
2019-09-16 16:16:48 +0300

    Update JsonTest_3.cpp

--- tests/JsonTest_3.cpp
+++ tests/JsonTest_3.cpp
@@ -7,7 +7,7 @@ TEST(JsonObject, EmptyObject)
 		Json json{"{}"};
 		EXPECT_EQ(json.is_object(), true);
 		EXPECT_EQ(json.is_array(), false);
-		EXPECT_EQ(json.is_null(), false);
+		EXPECT_EQ(json.is_empty(), false);
 	}
 
 TEST(JsonObject, SimpleObject)
@@ -15,7 +15,7 @@ TEST(JsonObject, SimpleObject)
 		Json json{R"({ "key" : "value" })"};
 		EXPECT_EQ(json.is_object(), true);
 		EXPECT_EQ(json.is_array(), false);
-		EXPECT_EQ(json.is_null(), false);
+		EXPECT_EQ(json.is_empty(), false);
 
 		EXPECT_EQ(std::any_cast<std::string>(json["key"]), "value");
 	}
@@ -25,12 +25,12 @@ TEST(JsonObject, ObjectInsideObject)
 		Json json{R"({ "key" : { "number":   9.5 } })"};
 		EXPECT_EQ(json.is_object(), true);
 		EXPECT_EQ(json.is_array(), false);
-		EXPECT_EQ(json.is_null(), false);
+		EXPECT_EQ(json.is_empty(), false);
 
 		Json &nested = *std::any_cast<Json *>(json["key"]);
 		EXPECT_EQ(nested.is_object(), true);
 		EXPECT_EQ(nested.is_array(), false);
-		EXPECT_EQ(nested.is_null(), false);
+		EXPECT_EQ(nested.is_empty(), false);
 
 		EXPECT_EQ(std::any_cast<double>(nested["number"]), 9.5);
 	}
@@ -40,12 +40,12 @@ TEST(JsonObject, ObjectdWithArray)
 		Json json{R"({ "key" : [1,2,3] })"};
 		EXPECT_EQ(json.is_object(), true);
 		EXPECT_EQ(json.is_array(), false);
-		EXPECT_EQ(json.is_null(), false);
+		EXPECT_EQ(json.is_empty(), false);
 
 		Json &nested = *std::any_cast<Json *>(json["key"]);
 		EXPECT_EQ(nested.is_object(), false);
 		EXPECT_EQ(nested.is_array(), true);
-		EXPECT_EQ(nested.is_null(), false);
+		EXPECT_EQ(nested.is_empty(), false);
 
 		EXPECT_EQ(std::any_cast<double>(nested[0]), 1);
 	}
@@ -55,12 +55,12 @@ TEST(JsonObject, ObjectsWithArrayAndValues)
 		Json json{R"({ "key" : [1,2,3], "key2": true })"};
 		EXPECT_EQ(json.is_object(), true);
 		EXPECT_EQ(json.is_array(), false);
-		EXPECT_EQ(json.is_null(), false);
+		EXPECT_EQ(json.is_empty(), false);
 
 		Json &nested = *std::any_cast<Json *>(json["key"]);
 		EXPECT_EQ(nested.is_object(), false);
 		EXPECT_EQ(nested.is_array(), true);
-		EXPECT_EQ(nested.is_null(), false);
+		EXPECT_EQ(nested.is_empty(), false);
 
 		EXPECT_EQ(std::any_cast<double>(nested[0]), 1);
 		EXPECT_EQ(std::any_cast<double>(nested[1]), 2);
