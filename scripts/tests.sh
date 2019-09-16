9ef4638da2fc760adc305d51caab0b608bf6ca90
MIB <48214233+DarthBarada@users.noreply.github.com>
2019-09-16 16:53:31 +0300

    Update tests.sh

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -9,4 +9,4 @@ CMAKE_CONFIG_OPTS="-DCMAKE_BUILD_TYPE=Debug"
 CMAKE_OPTS="$CMAKE_CONFIG_OPTS"
 cmake -H. -B .build $CMAKE_OPTS
 cmake --build .build
-cmake --build .build --target test -- ARGS="--verbose
+cmake --build .build --target test -- ARGS="--verbose"
