f087938ced8d8d36b80236cc6f130089234d620a
kelril <kelril@dsu1.service.sarnet>
2019-12-07 01:10:13 +0300

    fix

--- tests/JsonTest_1.cpp
+++ tests/JsonTest_1.cpp
@@ -3,13 +3,7 @@
 
 #include "Json.hpp"
 
-TEST(Json, NullJson)
-	{
-		Json json{};
-		EXPECT_EQ(json.is_object(), false);
-		EXPECT_EQ(json.is_array(), false);
-		EXPECT_EQ(json.is_empty(), true);
-	}
+
 
 TEST(Json, ExampleJson)
 	{
